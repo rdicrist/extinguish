e6c023861f68a81f7bfc1234f5f161f41b26a142
Billy Groble <wgroble@tulane.edu>
2018-01-14 23:33:13 +0000

    added validations for topic updates

--- app/controllers/subtopics_controller.rb
+++ app/controllers/subtopics_controller.rb
@@ -1,11 +1,14 @@
 class SubtopicsController < ApplicationController
     def create
-        @subtopic = Subtopic.new(subtopic_params)
-        if @subtopic.save
-            redirect_to "/tips", alert: "Subtopic added sucessfully!"
+        if current_user.admin
+            @subtopic = Subtopic.new(subtopic_params)
+            if @subtopic.save
+                redirect_to "/tips", alert: "Subtopic added sucessfully!"
+            else
+                redirect_to "/tips", alert: "Error adding subtopic. Don't forget to choose a topic."
+            end
         else
-            redirect_to "/tips", alert: "Error adding subtopic. Don't forget to choose a topic."
-            # maybe change to new_user_path
+            redirect_to "/tips", alert: "Error adding subtopic."
         end
     end
     
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,11 +1,14 @@
 class TopicsController < ApplicationController
     def create
-        @topic = Topic.new(topic_params)
-        if @topic.save
-            redirect_to "/tips", alert: "Topic added sucessfully!"
+        if current_user.admin
+            @topic = Topic.new(topic_params)
+            if @topic.save
+                redirect_to "/tips", alert: "Topic added sucessfully!"
+            else
+                redirect_to "/tips", alert: "Error adding topic."
+            end
         else
             redirect_to "/tips", alert: "Error adding topic."
-            # maybe change to new_user_path
         end
     end
     
